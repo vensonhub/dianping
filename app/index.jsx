2a9696141bfcd39908e2c78c71f6a88170876c3c
vensonhub <283747923@qq.com>
2017-07-11 00:14:59 +0800

    fetch koa

--- app/index.jsx
+++ app/index.jsx
@@ -6,6 +6,11 @@ import {Provider} from 'react-redux'
 import configureStore from './store/configureStore.js'
 import User from './containers/User'
 
+// 测试 fetch 的功能
+import { getData, postData } from './fetch/test.js'
+getData();
+postData();
+
 const store = configureStore();
 
 ReactDOM.render(
---
+++ mock/server.js
@@ -0,0 +1,32 @@
+var app = require('koa')();
+var router = require('koa-router')();
+var koaBody = require('koa-body')();
+
+router.get('/',function *(next){
+  this.body = 'hello koa';
+});
+
+router.get('/api',function *(next){
+  this.body='text data';
+});
+
+router.get('/api/1',function *(next){
+  this.body='test data 1';
+});
+
+router.get('/api/2',function *(next){
+  this.body={
+    a:1,
+    b:123
+  }
+});
+
+router.post('/api/post',koaBody,function *(next){
+  console.log(this.request.body);
+  this.body = JSON.stringify(this.request.body);
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
